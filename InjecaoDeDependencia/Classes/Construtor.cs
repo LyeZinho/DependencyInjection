8c5970ba848a99ba2b8a3e7e456f5cd030904ca9
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-05-14 22:20:28 +0100

    Adicionar arquivos de projeto.

---
+++ InjecaoDeDependencia/Classes/Construtor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InjecaoDeDependencia.Classes
+{
+    public interface IDbconect
+    {
+        dynamic GetData();
+    }
+
+    public class XDatabase : IDbconect
+    {
+        public dynamic GetData()
+        {
+            return new { name = "XDatabase" };
+        }
+    }
+
+    public class YDatabase : IDbconect
+    {
+        public dynamic GetData()
+        {
+            return new { name = "YDatabase" };
+        }
+    }
+
+    public class DbOperation
+    {
+        private IDbconect _dbconect;
+
+        public DbOperation(IDbconect dbconect)
+        {
+            _dbconect = dbconect;
+        }
+
+        public dynamic GetData()
+        {
+            return _dbconect.GetData();
+        }
+    }
+}
---
+++ InjecaoDeDependencia/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Injeção de Dependência é um é um padrão de projeto 
+usado para evitar o alto nível de acoplamento de código 
+dentro de uma aplicação. Sistemas com baixo acoplamento de 
+código são melhores pelos seguintes motivos:
+Aumento na facilidade de manutenção/implementação de novas 
+funcionalidades e também habilita a utilização de mocks para 
+realizar unit testes. 
+
+     Programe para uma interface e não para sua implementação
+
+*/
+
+
+using InjecaoDeDependencia.Classes;
+
+/*
+ * 
+ * Parametros: 
+ * new YDatabase()
+ * new XDatabase()
+ * 
+*/
+
+DbOperation db = new DbOperation(new YDatabase());
+
+Console.WriteLine(db.GetData().name);
